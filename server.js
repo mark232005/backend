645978af637a83594220f29847afedeceac1f013
mark <alufmark@gmail.com>
2025-04-29 11:49:27 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+
+
+import express from 'express'
+import { toyService } from './services/toy.service.js'
+import { loggerService } from './services/logger.service.js'
+
+const app=express()
+
+
+
+////////////////*Toys API*////////////////////////////////////
+app.get('/api/toy',(req,res)=>{
+    const { filterBy = {}, sortBy = {}} = req.query
+    toyService.query(filterBy,sortBy).then(
+        toys=>res.send(toys)).catch(
+            err=>{
+                loggerService.error('Cannot load toys',err)
+                res.status(400).send('Cannot load toys')
+            }
+        )
+
+})
+
+
+const port = process.env.PORT || 3030
+app.listen(port, () => {
+    loggerService.info(`Server listening on port http://127.0.0.1:${port}/`)
+})
